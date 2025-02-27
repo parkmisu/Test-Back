13a1177eae395f4f8a3769d3881986ce99279ef1
DESKTOP-33Q9NOJ\박미수 <altnrha@naver.com>
2025-02-27 17:46:11 +0900

    DB 연결 확인

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('MISU PARK API')
+    .setDescription('The User management API description')
+    .setVersion('1.0')
+    .addTag('user')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
+  await app.listen(3100);
 }
 bootstrap();
